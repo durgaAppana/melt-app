ae62e2ec8ca1aa20bba264421508ca47e284c8ab
durga.a <durga.a@fortune4.in>
2023-07-27 17:46:31 +0530

    css changes

--- components/common/pageNotFound.js
+++ components/common/pageNotFound.js
@@ -6,7 +6,7 @@ export default function PageNotFound() {
 		<div className="container page-not-found">
 			<h1>404 - Page Not Found</h1>
 			<h4>
-				Sorry, Nothing to show here. Please click  <Link href="/"> here</Link> to go t0 homepage
+				Sorry, Nothing to show here. Please click <Link href="/"> here</Link> to explore our articles.
 			</h4>
 		</div>
 	);
