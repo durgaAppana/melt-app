fc8d1311d012cfaf19d708fff843fc54a753445b
vikas God <vikas.g@fortune4.in>
2023-08-01 14:19:01 +0530

    seo

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import LandingWrapper from "../components/landing_wrapper/landingWrapper";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
+	const seoOption = {
+		title:"Melt - Marketing, media, advertising and technology",
+		description:"Melt - Marketing, media, advertising and technology"
+	}
 	return (
-		<div className={styles.container}>
-			<LandingWrapper />
-		</div>
+		<>
+		<NextSeo {...seoOption} />
+			<div className={styles.container}>
+				<LandingWrapper />
+			</div>
+		</>
 	);
 }
