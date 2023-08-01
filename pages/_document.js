4afd9a49b6c0329a20b49695feba0c39f72d631e
durga.a <durga.a@fortune4.in>
2023-08-01 18:08:12 +0530

    link changes

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,11 @@ class MyDocument extends Document {
 						content="Melt - Marketing, media, advertising and technology"
 						description="Melt - Marketing, media, advertising and technology"
 					/>
+					<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 					<link
-						rel="icon"
+						rel="preload"
 						href="/favicon.ico"
+						as="style"
 					/>
 				</Head>
 				<body>
