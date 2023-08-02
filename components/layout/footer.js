b6b7e16aab4a5665ae1d0449ca2c5c771b1cba5f
sayyednoman <sayyednoman@gmail.com>
2023-08-02 11:39:28 +0530

    ui fixes

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import footerStyle from "./../../styles/footer.module.css";
+import footerStyle from "./../../styles/footer.module.scss";
 import RegisterModal from "../common/registorModal";
 import Link from "next/link";
 
@@ -9,72 +9,63 @@ export default function Footer() {
 	return (
 		<div className={footerStyle["footer"]}>
 			<div className="container">
-				<div className="row">
-					<div className="col-md-10">
-						<div className="menu-footer">
-							<ul className={footerStyle["footer-nav"]}>
-								<li className={footerStyle["nav-item"]}>
-									<Link
-										href="/privacy-policy"
-										className={footerStyle["nav-link"]}
-									>
-										Privacy Policy
-									</Link>
-								</li>
-								<li className={footerStyle["nav-item"]}>
-									<Link
-										href="/advertiser-info"
-										className={footerStyle["nav-link"]}
-									>
-										Advertising Opportunities
-									</Link>
-								</li>
-								<li className={footerStyle["nav-item"]}>
-									<a
-										href="#"
-										className={footerStyle["nav-link"]}
-										onClick={toggle}
-									>
-										Register
-									</a>
-								</li>
-								<li className={footerStyle["nav-item"]}>
-									<Link
-										href="/contact-us"
-										className={footerStyle["nav-link"]}
-									>
-										Contact Us
-									</Link>
-								</li>
-								<li className={footerStyle["nav-item"]}>
-									<Link
-										href="/about-us"
-										className={footerStyle["nav-link"]}
-									>
-										About Us
-									</Link>
-								</li>
-								<li
-									className={footerStyle["nav-item"]}
-									style={{ borderRight: "none" }}
-								>
-									<Link
-										title="Terms and Conditions"
-										href="/help-and-information"
-										className={footerStyle["nav-link"]}
-									>
-										Terms and Conditions
-									</Link>
-								</li>
-							</ul>
-						</div>
-					</div>
-					<div className={"col-md-2 " + footerStyle["copy-right-text"]}> © Melt</div>
-					<RegisterModal
-						openModal={openModal}
-						toggle={toggle}
-					/>
-				</div>
+				<ul className={footerStyle["footer-nav"]}>
+					<li className={footerStyle["nav-item"]}>
+						<Link
+							href="/privacy-policy"
+							className={footerStyle["nav-link"]}
+						>
+							Privacy Policy
+						</Link>
+					</li>
+					<li className={footerStyle["nav-item"]}>
+						<Link
+							href="/advertiser-info"
+							className={footerStyle["nav-link"]}
+						>
+							Advertising Opportunities
+						</Link>
+					</li>
+					<li className={footerStyle["nav-item"]}>
+						<a
+							href="#"
+							className={footerStyle["nav-link"]}
+							onClick={toggle}
+						>
+							Register
+						</a>
+					</li>
+					<li className={footerStyle["nav-item"]}>
+						<Link
+							href="/contact-us"
+							className={footerStyle["nav-link"]}
+						>
+							Contact Us
+						</Link>
+					</li>
+					<li className={footerStyle["nav-item"]}>
+						<Link
+							href="/about-us"
+							className={footerStyle["nav-link"]}
+						>
+							About Us
+						</Link>
+					</li>
+					<li
+						className={footerStyle["nav-item"]}
+						style={{ borderRight: "none" }}
+					>
+						<Link
+							title="Terms and Conditions"
+							href="/help-and-information"
+							className={footerStyle["nav-link"]}
+						>
+							Terms and Conditions
+						</Link>
+					</li>
+				</ul>
+				<span className={footerStyle["copyright"]}>&copy; Melt</span>
+				<RegisterModal openModal={openModal} toggle={toggle} />
 			</div>
 		</div>
 	);
