5b3dca62663af69a0f4af337ad69b30b5d693023
durga.a <durga.a@fortune4.in>
2023-08-11 13:45:49 +0530

    contact us page changes

--- next-js/pages/contact-us.js
+++ next-js/pages/contact-us.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ContactWrapper from '../components/contact_us/contactWrapper'
 import { NextSeo } from 'next-seo'
 
-export default function index() {
+export default function Index() {
     const seoOption = {
 		title:"Contact Us -melt",
 		description:"Contact Us"
